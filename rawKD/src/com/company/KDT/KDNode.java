b071528ad77d82fe86665aba5efe10b4714c999c
Dawid Kosior <d4wt0n@gmail.com>
2020-03-22 17:04:41 +0100

    setting main

---
+++ rawKD/src/com/company/KDT/KDNode.java
@@ -0,0 +1,11 @@
+package com.company.KDT;
+
+public class KDNode {
+    Point2D point;
+    KDNode left;
+    KDNode right;
+
+    public double distance(Point2D dest){
+        return Math.sqrt((dest.x-this.point.x)*(dest.x-this.point.x) + (dest.y-this.point.y)*(dest.y-this.point.y));
+    }
+}
