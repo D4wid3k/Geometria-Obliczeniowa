68f41f6e0c5d4aa98d703f06db186ea651a96edb
Dawid Kosior <d4wt0n@gmail.com>
2020-03-22 17:05:56 +0100

    setting main

---
+++ rawKD/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import com.company.KDT.Point2D;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random generator = new Random();
+        Point2D point1 = new Point2D(generator.nextDouble()*100, generator.nextDouble()*100);
+        Point2D point2 = new Point2D(generator.nextDouble()*100, generator.nextDouble()*100);
+        Point2D point3 = new Point2D(generator.nextDouble()*100, generator.nextDouble()*100);
+        Point2D point4 = new Point2D(generator.nextDouble()*100, generator.nextDouble()*100);
+        Point2D point5 = new Point2D(generator.nextDouble()*100, generator.nextDouble()*100);
+        Point2D point6 = new Point2D(generator.nextDouble()*100, generator.nextDouble()*100);
+        Point2D point7 = new Point2D(generator.nextDouble()*100, generator.nextDouble()*100);
+        Point2D point8 = new Point2D(generator.nextDouble()*100, generator.nextDouble()*100);
+        Point2D point9 = new Point2D(generator.nextDouble()*100, generator.nextDouble()*100);
+        Point2D point10 = new Point2D(generator.nextDouble()*100, generator.nextDouble()*100);
+
+    }
+}
