0d43806ec0c36c94168cbb99c6f6c1455730d376
Dawid Kosior <d4wt0n@gmail.com>
2020-04-06 19:39:01 +0200

    final2

--- PROJEKT1/src/com/company/geoModels/Line.java
+++ PROJEKT1/src/com/company/geoModels/Line.java
@@ -20,17 +20,6 @@ public class Line {
         tab[1] = b;
         return tab;*/
     }
-    public static void translacja(Line l, int a, int b){
-        l.head.x += a;
-        l.head.y += b;
-        l.tail.x += a;
-        l.tail.y += b;
-    }
-    public static void obrot(Point l, double rad){
-        l.x = l.x * Math.cos(rad) - l.y * Math.sin(rad);
-        l.y = l.y * Math.cos(rad) - l.x * Math.sin(rad);
-        System.out.println(l.x + " " + l.y);
-    }
     public static void whichSide(Line l, Point p3){
         double a = ((l.head.y) - (l.tail.y)) / ((l.head.x) - (l.tail.x));
         double b = l.head.y - (a * l.head.x);
@@ -65,37 +54,5 @@ public class Line {
         y = wy / w;
 
         System.out.println("Wspolrzedne punktu przeciecia to ("+x+","+y+").");
-    }
-    public static void circleLine(){
-        System.out.println();
-        System.out.println("Zadanie 4");
-
-        Random gen = new Random();
-        Point p1 = new Point(gen.nextInt(300),gen.nextInt(300));
-        Point p2 = new Point(gen.nextInt(300),gen.nextInt(300));
-
-        Point cenK = new Point(150,150);
-        double r = 50;
-        double a = p1.x*p1.x + p1.y*p1.y + p2.x*p2.x + p2.y*p2.y - 2*(p1.x*p2.x + p1.y*p2.y);
-        double b = 2*(cenK.x*(p2.x-p1.x)+cenK.y*(p2.y-p1.y)+p1.x*p2.x+p1.y*p2.y-(p2.x*p2.x)-(p2.y*p2.y));
-        double c = -(r*r) + p2.x*p2.x + p2.y*p2.y + cenK.x*cenK.x + cenK.y*cenK.y - 2*(cenK.x*p2.x + cenK.y*p2.y);
-
-        double delta = b*b - 4*a*c;
-        if(delta>0){
-            double x1 = (-b - Math.sqrt(delta))/2*a;
-            double x2 = (-b + Math.sqrt(delta))/2*a;
-
-            double y1 = a*x1 + b;
-            double y2 = a*x2 + b;
-
-            System.out.println("Punkty przeciecia to: ("+x1+","+y1+") ("+x2+","+y2+")");
-        }else if(delta == 0){
-            double x = -b /2*a;
-            double y = a*x + b;
-
-            System.out.println("Punkty przeciecia to: ("+x+","+y+")");
-        }else{
-            System.out.println("Nie ma punktow przeciecia.");
-        }
     }
 }
